bd742305bd71dbc4691568e56de04c8757509d8f
JavierLopez <mail.jlc98@gmail.com>
2021-04-30 13:37:24 +0200

    Commit update/create

--- MvcCoreAWSPractica/Controllers/CochesController.cs
+++ MvcCoreAWSPractica/Controllers/CochesController.cs
@@ -22,5 +22,25 @@ namespace MvcCoreAWSPractica.Controllers
         {
             return View(this.repo.BuscarCoche(id));
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(String marca, String modelo, String conductor, String imagen)
+        {
+            this.repo.NuevoCoche(marca, modelo, conductor, imagen);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Update(int id)
+        {
+            return View(this.repo.BuscarCoche(id));
+        }
+        [HttpPost]
+        public IActionResult Update(int id,String marca, String modelo, String conductor, String imagen)
+        {
+            this.repo.modificarCoche(id, marca, modelo, conductor, imagen);
+            return RedirectToAction("Index");
+        }
     }
 }
--- MvcCoreAWSPractica/Repositories/RepositoryCoches.cs
+++ MvcCoreAWSPractica/Repositories/RepositoryCoches.cs
@@ -22,5 +22,29 @@ namespace MvcCoreAWSPractica.Repositories
         {
             return this.context.Coches.SingleOrDefault(x => x.Idcoche == id);
         }
+        public void NuevoCoche(String marca,String modelo,String conductor,String imagen)
+        {
+            Coche car = new Coche();
+            car.Idcoche = this.GetMaxId();
+            car.Marcar = marca;
+            car.Modelo = modelo;
+            car.Conductor = conductor;
+            car.Imagen = imagen;
+            this.context.Coches.Add(car);
+            this.context.SaveChanges();
+        }
+        public int GetMaxId()
+        {
+            return this.context.Coches.Max(x => x.Idcoche) + 1;
+        }
+        public void modificarCoche(int id, String marca, String modelo, String conductor, String imagen)
+        {
+            Coche car = this.BuscarCoche(id);
+            car.Marcar = marca;
+            car.Modelo = modelo;
+            car.Conductor = conductor;
+            car.Imagen = imagen;
+            this.context.SaveChanges();
+        }
     }
 }
